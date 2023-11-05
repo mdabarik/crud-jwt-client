8a2f05a34b8cdcb828b7e2c4d2bca7dd4cf43039
mdabarik <mdabarik19@gmail.com>
2023-11-06 00:05:46 +0600

    axios hook added

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,18 +1,30 @@
 import { Link, NavLink } from "react-router-dom";
 import { CiLogin } from "react-icons/ci";
+import { FaHome } from "react-icons/fa";
+import { GiHomeGarage } from "react-icons/gi";
+import { BsFillCartCheckFill } from "react-icons/bs";
 import "./NavBar.css";
 
 const NavBar = () => {
     return (
         <>
             <li>
-                <NavLink to="/">Home</NavLink>
+                <NavLink to="/">
+                    <FaHome></FaHome>
+                    <span>Home</span>
+                </NavLink>
             </li>
             <li>
-                <NavLink to="/rooms">Rooms</NavLink>
+                <NavLink to="/rooms">
+                    <GiHomeGarage></GiHomeGarage>
+                    <span>Rooms</span>
+                </NavLink>
             </li>
             <li>
-                <NavLink to="/my-booking">My Booking</NavLink>
+                <NavLink to="/my-booking">
+                    <BsFillCartCheckFill></BsFillCartCheckFill>
+                    <span>My Booking</span>
+                </NavLink>
             </li>
             <li>
                 <Link to="/login" className="px-3 py-2 bg-[#db332a] rounded-sm normal-case flex gap-1 text-white border-none hover:bg-[#b5100b] hover:text-white">
---
+++ src/hooks/useAxios.jsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: 'http://localhost:5555',
+    withCredentials: true
+})
+
+const useAxios = () => {
+    instance.interceptors.response.use(
+        (response) => {
+            response
+        },
+        (error) => {
+            if (error.response.status == 401 || error.response.status == 403) {
+                // logout
+            }
+        }
+    )
+    return instance;
+};
+
+export default useAxios;
