02e25754d77bdd4a00d65912f831b25d1c728e03
mdabarik <mdabarik19@gmail.com>
2023-11-06 02:10:20 +0600

    rooms page functionality added

--- src/components/RoomCard/RoomCard.jsx
+++ src/components/RoomCard/RoomCard.jsx
@@ -1,9 +1,20 @@
+import { useNavigate } from "react-router-dom";
 
-const RoomCard = ({room}) => {
-    const {price_per_night, room_images, reviews, reviewCount} = room || {};
+const RoomCard = ({ room }) => {
+    const navigate = useNavigate();
+    const { _id, price_per_night, room_images, countStars, countReviews } = room || {};
     return (
-        <div>
-             <div className="max-w-[300px] h-[200px] bg-gray-400"></div>
+        <div className="hover:cursor-pointer" onClick={() => navigate(_id)}>
+            <div>
+                <img className="w-full rounded-t-lg h-[200px] object-cover" src={room_images[0]} alt="room image" />
+            </div>
+            <div className="w-full h-[100px] bg-gray-400 rounded-b-lg">
+                <p>Price per night: ${price_per_night}</p>
+                <div>
+                    Rating: * * * * * (5)
+                </div>
+            </div>
+
         </div>
     );
 };
--- src/pages/Rooms/Rooms.jsx
+++ src/pages/Rooms/Rooms.jsx
@@ -4,14 +4,19 @@ import RoomCard from "../../components/RoomCard/RoomCard";
 
 const Rooms = () => {
     const axios = useAxios();
-
+    const [range, setRange] = useState('');
+    const [sortOrder, setSortOrder] = useState('')
     const [rooms, setRooms] = useState([]);
+
     useEffect(() => {
-        axios.get('/api/v1/rooms')
-        .then(res => {
-            setRooms(res.data);
-        })
-    }, [])
+        // http://localhost:5555/api/v1/rooms?filterByPrice=0-100&sortField=price_per_night&sortOrder=asc
+        const url = `/api/v1/rooms?filterByPrice=${range}&sortField=price_per_night&sortOrder=${sortOrder}`;
+        console.log(url);
+        axios.get(url)
+            .then(res => {
+                setRooms(res.data);
+            })
+    }, [range, sortOrder])
 
     return (
         <div className="my-8">
@@ -20,31 +25,37 @@ const Rooms = () => {
                 <h2 className="text-2xl font-bold">Filter Rooms</h2>
                 <div>
                     <p>Sort By Range:</p>
-                    <select onChange={() => {}} className="select select-bordered w-full max-w-xs">
-                        <option value="a" disabled selected>Select One</option>
-                        <option value="b">$0-$100</option>
-                        <option value="c">$101-$200</option>
-                        <option value="d">$201-$400</option>
-                        <option value="e">$401+</option>
+                    <select onChange={(e) => {
+                        setRange(e.target.value)
+                    }} className="select select-bordered w-full max-w-xs">
+                        <option value={""} selected>Show All</option>
+                        <option value="0-100">$0-$100</option>
+                        <option value="101-200">$101-$200</option>
+                        <option value="201-400">$201-$400</option>
+                        <option value="401-10000">$401+</option>
                     </select>
                 </div>
                 <div>
                     <p>Sort By Price:</p>
-                    <select  onChange={() => {}} className="select select-bordered w-full max-w-xs">
-                        <option value="aa" disabled selected>Select One</option>
+                    <select onChange={(e) => { setSortOrder(e.target.value) }} className="select select-bordered w-full max-w-xs">
+                        <option value={""} selected>No Sort</option>
                         <option value={"asc"}>Low to High</option>
                         <option value={"desc"}>High to Low</option>
                     </select>
                 </div>
             </div>
-           {/* Rooms */}
-           <div className="my-8 border-2">
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-                    {
-                        rooms.map(room => <RoomCard key={room._id} room={room}></RoomCard>)
-                    }
-                </div>
-           </div>
+            {/* Rooms */}
+            <div className="my-8">
+                {
+                    rooms.length == 0 ?
+                        <h2 className="text-center text-3xl font-bold text-red-700">No rooms found</h2> :
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+                            {
+                                rooms.map(room => <RoomCard key={room._id} room={room}></RoomCard>)
+                            }
+                        </div>
+                }
+            </div>
         </div>
     );
 };
