dc352630944c81c983a8bfba14d55503ca18ea61
mdabarik <mdabarik19@gmail.com>
2023-11-06 13:47:11 +0600

    logout added

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,9 +3,27 @@ import { CiLogin } from "react-icons/ci";
 import { FaHome } from "react-icons/fa";
 import { GiHomeGarage } from "react-icons/gi";
 import { BsFillCartCheckFill } from "react-icons/bs";
+import { AiOutlineLogout } from "react-icons/ai";
 import "./NavBar.css";
+import { useContext } from "react";
+import { GlobalContext } from "../../providers/GlobalProvider";
+import toast from "react-hot-toast";
 
 const NavBar = () => {
+
+    const { user, logOut } = useContext(GlobalContext);
+
+    const handleLogOut = () => {
+        logOut()
+        .then(res => {
+            toast.success("Logout successfull")
+            console.log(res);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
+
     return (
         <>
             <li>
@@ -27,10 +45,26 @@ const NavBar = () => {
                 </NavLink>
             </li>
             <li>
-                <Link to="/login" className="px-3 py-2 bg-[#db332a] rounded-sm normal-case flex gap-1 text-white border-none hover:bg-[#b5100b] hover:text-white">
-                    <CiLogin className="text-2xl"></CiLogin>
-                    <span>Login</span>
-                </Link>
+                {
+                    !user ?
+                        <Link to="/login" className="px-3 py-2 bg-[#db332a] rounded-sm normal-case flex gap-1 text-white border-none hover:bg-[#b5100b] hover:text-white">
+                            <CiLogin className="text-2xl"></CiLogin>
+                            <span>Login</span>
+                        </Link>
+                        :
+                        <div className="flex items-center gap-2">
+                            <div className="flex flex-col">
+                                <img className="rounded-full w-[46px] h-[46px]" src={user?.profileUrl} alt="profile" />
+                                <p>{user?.displayName}</p>
+                            </div>
+                            <Link onClick={handleLogOut} className="px-4 py-3 bg-[#db332a] rounded-full normal-case flex gap-1 text-white border-none hover:bg-[#b5100b] hover:text-white">
+                                <AiOutlineLogout></AiOutlineLogout>
+                                <span>Logout</span>
+                            </Link>
+                        </div>
+                }
+
+
             </li>
         </>
     );
