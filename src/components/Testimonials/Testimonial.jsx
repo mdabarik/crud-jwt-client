17e998f8a38e3ffe3f8300be6fbc0a756623ce8f
mdabarik <mdabarik19@gmail.com>
2023-11-07 17:15:13 +0600

    testimonial reorganized

--- src/components/Testimonials/Testimonial.jsx
+++ src/components/Testimonials/Testimonial.jsx
@@ -1,14 +1,16 @@
 import Rating from '@mui/material/Rating';
 import StarIcon from '@mui/icons-material/Star';
+import BannerReview from "../../assets/bannerReview.jpg";
 
 const Testimonial = ({ testitmonial }) => {
-    const { name, img, rating, date, review, profession } = testitmonial || {};
+    const { name, photoURL, rating, date, review, profession } = testitmonial || {};
 
     return (
         <div className="text-white h-[350px] flex flex-col items-center justify-center rounded-lg relative">
-            <div className="absolute top-0 left-0 bg-[#00000090] w-full h-full rounded-lg"></div>
-            <div className="absolute top-0 left-0 h-full flex flex-col items-center justify-center px-8 ">
-                <img className='w-[75px] md:w-[100px] h-[75px] mb-2 md:h-[100px] object-cover rounded-full' src={img} alt={name} />
+            <img src={BannerReview} alt="banner bg" />
+            <div className="absolute top-0 left-0 bg-[#00000099] w-full h-full rounded-lg"></div>
+            <div className="absolute top-0 left-0 h-full flex flex-col items-center justify-center px-8 w-full">
+                <img className='w-[75px] md:w-[100px] h-[75px] mb-2 md:h-[100px] object-cover rounded-full z-50' src={photoURL} alt={name} />
                 <h1 className="font-bold text-[16px] md:text-2xl mb-1 text-center">
                     {name}
                 </h1>
@@ -18,7 +20,7 @@ const Testimonial = ({ testitmonial }) => {
                 <h3 className="text-[11px] md:text-sm mb-2 md:mb-3">
                     Posted on: {date || "12/23/2023"}
                 </h3>
-                <div>
+                <div className=' w-4/6'>
                     <p className="text-center text-[10px] md:text-sm">{review}</p>
                 </div>
                 <div className="mt-4">
