e0e8e5596737e6c81fb3575a24c859c97f640947
mdabarik <mdabarik19@gmail.com>
2023-11-08 16:26:33 +0600

    final touch

--- src/components/Sliders/Slider.jsx
+++ src/components/Sliders/Slider.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 const Slider = ({slider}) => {
     const { title, subtitle, image } = slider || {};
@@ -8,7 +9,7 @@ const Slider = ({slider}) => {
             <div className="h-[500px] w-full bg-[#00000090] absolute top-0 left-0 flex  flex-col items-center justify-center space-y-5">
                 <h1 className="text-center text-white text-2xl md:text-3xl lg:text-4xl font-bold">{title}</h1>
                 <p className="text-sm md:text-lg text-center text-white">{subtitle}</p>
-                <button className="btn btn-secondary text-center" >Buy Now</button>
+                <Link to="/rooms" className="btn btn-secondary text-center" >Book Now</Link>
             </div>
         </div>
     )
